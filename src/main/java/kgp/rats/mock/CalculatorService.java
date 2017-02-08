cd8ee2bf2f97f782e68d4c09f367393ab0ba6b54
pgopalakrishnan <pgopalakrishnan@ebay.com>
2017-02-08 14:20:53 -0600

    Initial commit

---
+++ src/main/java/kgp/rats/mock/CalculatorService.java
@@ -0,0 +1,8 @@
+package kgp.rats.mock;
+
+public interface CalculatorService {
+	public double add(double n1, double n2);
+	public double subtract(double n1, double n2);
+	public double multiply(double n1, double n2);
+	public double divide(double n1, double n2);
+}
---
+++ src/test/java/kgp/rats/MathApplicationTester.java
@@ -0,0 +1,30 @@
+package kgp.rats;
+
+import kgp.rats.mock.CalculatorService;
+import kgp.rats.mock.MathApplication;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
+
+	@InjectMocks
+	private MathApplication mathApplication;
+	
+	@Mock
+	private CalculatorService calculatorService;
+	
+	@Test
+	public void testAdd(){
+		when(calculatorService.add(20, 30)).thenReturn(50d);
+		Assert.assertEquals(50, mathApplication.add(20, 30),0);
+	}
+	
+}
